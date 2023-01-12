7b09440fa3f79d67aa8ab1158c489f8ec786db49
GomezFrannco <frangomez.baires@gmail.com>
2023-01-11 23:02:49 -0300

    feat: Add mongodb connection

---
+++ server/src/utils/db.ts
@@ -0,0 +1,18 @@
+import config from 'config'
+import mongoose from 'mongoose'
+import logger from './logger'
+
+export async function connectToDB() {
+  try {
+    await mongoose.connect(config.get('dbHost'))
+  } catch (e) {
+    logger.error('Error connecting to database', e)
+    process.exit(1)
+  }
+}
+
+export async function disconnectFromDB() {
+  await mongoose.connection.close()
+  logger.info('Disconnecting from database')
+  return
+}
