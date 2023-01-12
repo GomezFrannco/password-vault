8cd8ff09527a8353fcca296506e9e709afbf298c
GomezFrannco <frangomez.baires@gmail.com>
2023-01-12 17:08:23 -0300

    feat: Add user register route

--- server/src/modules/user/user.route.ts
+++ server/src/modules/user/user.route.ts
@@ -1,10 +1,14 @@
 import { FastifyInstance, FastifyPluginOptions, FastifyError } from 'fastify'
+import { registerUserHandler } from './user.controller'
 
 function userRoutes(
   app: FastifyInstance,
   _: FastifyPluginOptions,
   done: (err?: FastifyError) => void
 ) {
+
+  app.post('/register', registerUserHandler)
+
   done()
 }
 
