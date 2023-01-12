d9f757c60477cdc6cd77b9a31809c16051c8513d
GomezFrannco <frangomez.baires@gmail.com>
2023-01-12 16:57:03 -0300

    feat: Add cookieDomain

--- server/config/default.ts
+++ server/config/default.ts
@@ -4,6 +4,7 @@ export default {
   port: process.env.PORT,
   dbHost: process.env.MONGO_HOST,
   corsOrigin: process.env.CORS_ORIGIN,
+  cookieDomain: process.env.COOKIE_DOMAIN,
   privateKey: process.env.ACCESS_PRIVATE_KEY,
   publicKey: process.env.ACCESS_PUBLIC_KEY,
 }
